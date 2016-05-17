3cad1676e1f40f8707d6bc6b8b3a9f47a95b4d29
Jennifer Kuhnert <jennifer.kuhnert@vacom.de>
2016-05-17 13:37:58 +0200

    push

---
+++ app/src/main/java/com/may/amy/piqz/util/PrivateConstants.java
@@ -0,0 +1,14 @@
+package com.may.amy.piqz.util;
+
+/**
+ * Created by kuhnertj on 27.04.2016.
+ */
+public class PrivateConstants {
+    public static final String CLIENT_ID = "7C_fKH9VKLR6ow";
+    public static final String REDIRECT_URI = "http://amylinn.github.io/piqz";
+    public static final String URL_USER = "/user/missevrymay";
+    public static final String URL_MULTI = "/m/forthelulz";
+
+
+    public static final String TEST_DEVICE_S4 = "367A3FB8F740C4C8990017523559392C";
+}
